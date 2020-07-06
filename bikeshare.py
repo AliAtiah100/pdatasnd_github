5a12c28eecf41624fc9355420c07691cf5839c62
Ali Alzahrani <aliatiah100@gmail.com>
2020-07-06 17:43:35 +0300

    Edit the final message by adding credit to Udacity

--- bikeshare.py
+++ bikeshare.py
@@ -210,7 +210,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no. \n ').lower()
         if restart.lower() != 'yes':
 
-            print('\nThank you for using the app.\n ')
+            print('\nThank you for using the app. This code supported by Udacity\n ')
             break
 
 if __name__ == "__main__":
